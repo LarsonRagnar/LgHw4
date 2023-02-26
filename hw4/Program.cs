22a61ed9858d9ad2c8a48ec4765ed988778c6256
LarsonRagnar <larionov1992@gmail.com>
2023-02-26 21:02:13 +0300

    3 task!

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -39,4 +39,24 @@
 // int[] resultArray = GetBinaryArray(8);
 // Console.WriteLine($"Массив:  [{String.Join(";", resultArray)}]");
 
+// Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+// Console.WriteLine("Введите число A ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int C = 1;
+// for (int i = 0; i < B; i++)
+// {
+//     C *= A;
+// }
+// Console.WriteLine($"Число {A} в Степени числа {B} будет {C} ");
+
+
 
